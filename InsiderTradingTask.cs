429df4f6395664bad0b8b3208c94c1355a4347c2
Tim Hanewich <tahanewich@live.com>
2021-07-27 10:17:30 -0400

    Added status reporting to insider trading task

--- InsiderTradingTask.cs
+++ InsiderTradingTask.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using Yahoo.Finance;
+using System.Collections.Generic;
 
 namespace Insider_Trading_Bot
 {
@@ -15,6 +16,36 @@ namespace Insider_Trading_Bot
         //The two metrics that will trigger a sell off - in other words, these are the two outs to the holding. If either of these turn true, the stock will be sold.
         public float AnticipatedVolumePercentJump {get; set;} //The percent jump in volume that is anticipated as a result of the insider trading news. After the volume goes up by this amount, the shares will be sold.
         public float AnticipatedPricePercentJump {get; set;} //The percent jump in price that will cause a sell off.
+
+
+        public InsiderTradingTask()
+        {
+            Statuses = new List<string>();
+        }
+
+
+        #region "Status reporting"
+
+        private List<string> Statuses;
+
+        public void AddStatus(string status)
+        {
+            Statuses.Insert(0, status);
+
+            //If there are more than 20, trim
+            while (Statuses.Count > 20)
+            {
+                Statuses.RemoveAt(Statuses.Count - 1);
+            }
+        }
+
+        public string[] GetStatuses()
+        {
+            return Statuses.ToArray();
+        }
+
+        #endregion
+
 
         public async Task<float> GetAverageVolumePerMinuteAsync(string symbol)
         {
