14fa8967ccd66f6788ca02f4568b24ba8d40c329
Tim Hanewich <tahanewich@live.com>
2021-08-01 13:19:09 -0400

    Used only admin write line

--- InsiderTradingBot.cs
+++ InsiderTradingBot.cs
@@ -12,7 +12,7 @@ namespace Insider_Trading_Bot
                 SimulatedPortfolio Portfolio = PortfolioCloudInterface.DownloadPortfolioAsync().Result;
                 Portfolio.TradeEquityAsync(symbol, quantity, tt).Wait();
                 PortfolioCloudInterface.UploadPortfolioAsync(Portfolio).Wait();
-                Console.WriteLine("Executed trade: " + quantity.ToString("#,##0") + " shares of " + symbol.ToUpper() + " - " + tt.ToString());
+                Program.AdminWriteLine("Executed trade: " + quantity.ToString("#,##0") + " shares of " + symbol.ToUpper() + " - " + tt.ToString());
             }
             catch (Exception ex)
             {
